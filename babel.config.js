0998a830f0b686865d05eed13833a5c7ca149c70
manInit <info.denisbronnikov@gmail.com>
2021-06-25 16:48:13 +0800

    init

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+const plugins = [];
+
+module.exports = {
+	presets: [
+		'@babel/preset-env', 
+		['@babel/preset-react', {
+			'runtime': 'automatic'
+		}]
+	],
+	plugins: plugins
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles/index.sass';
+
+
+console.log('hello world');
